e1a5f524836e19f6b70ebefd80bfad4b9fd4ddc6
Stefan Langenbach <stefan@langenbach.online>
2022-11-15 15:32:33 +0100

    Add config.fish

---
+++ config.fish
@@ -0,0 +1,4 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+end
